9d5da44ee53fee6214e5013610422b010cbc7476
Alberto Giusti <albertogiusti.giusti@gmail.com>
2021-05-12 18:10:32 +0200

    Refactor: synchronizer documentation. feat: rviz configuration

--- scout_odometry/src/integrator.cpp
+++ scout_odometry/src/integrator.cpp
@@ -17,28 +17,20 @@
  * This class represents the entire integrator node.
  *
  * It reads from /scout_twist, and for each geometry_msgs/TwistStamped.msg message received publishes on two topics:
- *  - /scout_odom       A nav_msgs/Odometry.msg message containing position and twist
- *  - /scout_state      A scout_odometry/State.msg message containing odometry and integration method
- * Moreover it publishes through a transform broadcaster:
- *  - /tf               A geometry_msgs/TransformStamped message containing the transform between robot and world frame
+ *  - scout_odom       A nav_msgs/Odometry.msg message containing position and twist
+ *  - scout_state      A scout_odometry/State.msg message containing odometry and integration method
+ * Moreover it publishes through a transform broadcaster the tf between robot and "odom" frame
  *
  * It also provides services to reset the robot pose and to set it to a desired value,
  * as well as a reconfigurable parameter /integration_method that can be set to Euler or Runge_Kutta.
  */
-class Integrator{
+class IntegratorNode{
 
     public:
 
     enum class Method {euler, rk};
 
-    explicit Integrator();
-    void setMethod(Method _method);
-    void integrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped);
-    void eulerIntegrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped);
-    void rkIntegrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped);
-    void publish(ros::Time stamp, geometry_msgs::Twist twist, geometry_msgs::Point position, double theta);
-    bool resetPose(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
-    bool setPose(scout_odometry::SetPose::Request& req, scout_odometry::SetPose::Request& res);
+    IntegratorNode();
 
     private:
 
@@ -49,29 +41,37 @@ class Integrator{
     ros::ServiceServer resetServer;
     ros::ServiceServer setServer;
     tf2_ros::TransformBroadcaster br;
+    dynamic_reconfigure::Server<scout_odometry::parametersConfig> server;
 
     Method method;
     ros::Time lastStamp;
     geometry_msgs::Twist lastTwist;
     geometry_msgs::Point lastPosition;
     double lastTheta;
+
+    void integrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped);
+    void eulerIntegrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped);
+    void rkIntegrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped);
+    void publish(ros::Time stamp, geometry_msgs::Twist twist, geometry_msgs::Point position, double theta);
+    bool resetPose(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res);
+    bool setPose(scout_odometry::SetPose::Request& req, scout_odometry::SetPose::Request& res);
+    void setMethod(scout_odometry::parametersConfig& config, uint32_t level);
 };
 
 /**
  * The class constructor looks for an initial pose in the parameter server,
  * subscribes to topics and advertises topics/services.
  */
-Integrator::Integrator()
-    : nodeHandle()
-    , lastStamp(ros::Time::now())
-    , subscriber(nodeHandle.subscribe("scout_twist", 1000, &Integrator::integrate, this))
+IntegratorNode::IntegratorNode()
+    : lastStamp(ros::Time::now())
+    , subscriber(nodeHandle.subscribe("scout_twist", 1000, &IntegratorNode::integrate, this))
     , odomPublisher(nodeHandle.advertise<nav_msgs::Odometry>("scout_odom", 1000))
     , statePublisher(nodeHandle.advertise<scout_odometry::State>("scout_state", 1000))
-    , resetServer(nodeHandle.advertiseService("reset_pose", &Integrator::resetPose, this))
-    , setServer(nodeHandle.advertiseService("set_pose", &Integrator::setPose, this))
-    , lastPosition()
-    , lastTheta(0)
+    , resetServer(nodeHandle.advertiseService("reset_pose", &IntegratorNode::resetPose, this))
+    , setServer(nodeHandle.advertiseService("set_pose", &IntegratorNode::setPose, this))
     , method(Method::euler){
+
+    server.setCallback(boost::bind(&IntegratorNode::setMethod, this, _1, _2));
 
     nodeHandle.getParam("initial_pose/x", lastPosition.x);
     nodeHandle.getParam("initial_pose/y", lastPosition.y);
@@ -80,19 +80,10 @@ Integrator::Integrator()
 }
 
 /**
- * Setter for the integration method enum attribute.
- * @param _method       The desired method
- */
-void Integrator::setMethod(Integrator::Method _method) {
-
-    method = _method;
-}
-
-/**
  * Integrates the robot position, choosing the right integration type based on the method attribute.
  * @param twistStamped      The current twist of the robot, along with a timestamp
  */
-void Integrator::integrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped) {
+void IntegratorNode::integrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped) {
 
     switch (method) {
         case Method::euler :
@@ -101,16 +92,14 @@ void Integrator::integrate(const geometry_msgs::TwistStamped::ConstPtr& twistSta
         case Method::rk :
             rkIntegrate(twistStamped);
     }
-
 }
 
 /**
  * Integrates the robot position using Euler integration.
  * @param twistStamped      The current twist of the robot, along with a timestamp
  */
-void Integrator::eulerIntegrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped) {
+void IntegratorNode::eulerIntegrate(const geometry_msgs::TwistStamped::ConstPtr& twistStamped) {
 
-    ROS_INFO("Euler integrating...");
     ros::Time stamp = twistStamped->header.stamp;
     ros::Duration deltaStamp = stamp - lastStamp;
 
@@ -134,9 +123,8 @@ void Integrator::eulerIntegrate(const geometry_msgs::TwistStamped::ConstPtr& twi
  * Integrates the robot position using Runge-Kutta integration.
  * @param twistStamped      The current twist of the robot, along with a timestamp
  */
-void Integrator::rkIntegrate(const geometry_msgs::TwistStamped::ConstPtr &twistStamped) {
+void IntegratorNode::rkIntegrate(const geometry_msgs::TwistStamped::ConstPtr &twistStamped) {
 
-    ROS_INFO("Runge-Kutta integrating...");
     ros::Time stamp = twistStamped->header.stamp;
     ros::Duration deltaStamp = stamp - lastStamp;
 
@@ -154,7 +142,6 @@ void Integrator::rkIntegrate(const geometry_msgs::TwistStamped::ConstPtr &twistS
     lastTwist = twist;
     lastPosition = position;
     lastTheta = theta;
-
 }
 
 /**
@@ -162,24 +149,22 @@ void Integrator::rkIntegrate(const geometry_msgs::TwistStamped::ConstPtr &twistS
  * @param stamp         The current timestamp
  * @param twist         The current twist
  * @param position      The current position
- * @param theta         The current orientation, measured as the angle around the z-axis between the robot and world frames
+ * @param theta         The current orientation, measured as the angle around the z-axis between the robot and fixed frames
  */
-void Integrator::publish(ros::Time stamp, geometry_msgs::Twist twist, geometry_msgs::Point position, double theta) {
+void IntegratorNode::publish(ros::Time stamp, geometry_msgs::Twist twist, geometry_msgs::Point position, double theta) {
 
-    ROS_INFO("Publishing stamp=%f twist.x=%f twist.z=%f position.x=%f position.y=%f theta=%f", stamp.toSec(), twist.linear.x, twist.angular.z, position.x, position.y, theta);
     tf2::Quaternion q;
     q.setRPY(0, 0, theta);
     q.normalize();
-    ROS_INFO("theta=%f x=%f y=%f z=%f w=%f", theta, q.x(), q.y(), q.z(), q.w());
     geometry_msgs::Quaternion orientation;
     orientation.x = q.x();
     orientation.y = q.y();
     orientation.z = q.z();
     orientation.w = q.w();
-    ROS_INFO("x=%f y=%f z=%f w=%f", orientation.x, orientation.y, orientation.z, orientation.w);
 
     nav_msgs::Odometry odom;
     odom.header.stamp = stamp;
+    odom.header.frame_id = "odom";
     odom.twist.twist = twist;
     odom.pose.pose.position = position;
     odom.pose.pose.orientation = orientation;
@@ -187,7 +172,7 @@ void Integrator::publish(ros::Time stamp, geometry_msgs::Twist twist, geometry_m
 
     geometry_msgs::TransformStamped transformStamped;
     transformStamped.header.stamp = stamp;
-    transformStamped.header.frame_id = "world";
+    transformStamped.header.frame_id = "odom";
     transformStamped.child_frame_id = "scout";
     transformStamped.transform.translation.x = position.x;
     transformStamped.transform.translation.y = position.y;
@@ -199,7 +184,6 @@ void Integrator::publish(ros::Time stamp, geometry_msgs::Twist twist, geometry_m
     state.method = (method == Method::euler) ? "euler" : "rk";
 
     statePublisher.publish(state);
-
 }
 
 /**
@@ -209,14 +193,14 @@ void Integrator::publish(ros::Time stamp, geometry_msgs::Twist twist, geometry_m
  * @param res       The service response (none)
  * @return          Always true
  */
-bool Integrator::resetPose(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res){
+bool IntegratorNode::resetPose(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res){
 
     lastPosition.x = 0;
     lastPosition.y = 0;
     lastTheta = 0;
     lastTwist.linear.x = 0;
     lastTwist.angular.z = 0;
-    ROS_INFO("ResetPose request received: x=%f, y=%f, theta=%f", lastPosition.x, lastPosition.y, lastTheta);
+    ROS_INFO("ResetPose request received. New pose: x=%f, y=%f, theta=%f", lastPosition.x, lastPosition.y, lastTheta);
     return true;
 }
 
@@ -227,49 +211,42 @@ bool Integrator::resetPose(std_srvs::Empty::Request& req, std_srvs::Empty::Respo
  * @param res       The service response (none)
  * @return          Always true
  */
-bool Integrator::setPose(scout_odometry::SetPose::Request& req, scout_odometry::SetPose::Request& res){
+bool IntegratorNode::setPose(scout_odometry::SetPose::Request& req, scout_odometry::SetPose::Request& res){
 
     lastPosition.x = req.x;
     lastPosition.y = req.y;
     lastTheta = req.theta;
     lastTwist.linear.x = 0;
     lastTwist.angular.z = 0;
-    ROS_INFO("SetPose request received: x=%f, y=%f, theta=%f", lastPosition.x, lastPosition.y, lastTheta);
+    ROS_INFO("SetPose request received. New pose: x=%f, y=%f, theta=%f", lastPosition.x, lastPosition.y, lastTheta);
     return true;
 }
 
 /**
  * The callback function for the dynamic_reconfigure server.
- * It changes Integrator::method according to the selected integration_method parameter.
+ * It changes IntegratorNode::method according to the selected integration_method parameter.
  * @param integrator    The integrator instance to be modified
  * @param config        The config containing the method choice
  * @param level         The dynamic_reconfigure level bitmask
  */
-void setMethod(Integrator& integrator, scout_odometry::parametersConfig& config, uint32_t level) {
+void IntegratorNode::setMethod(scout_odometry::parametersConfig& config, uint32_t level) {
 
     if (config.integration_method == 0) {
-        integrator.setMethod(Integrator::Method::euler);
+        method = Method::euler;
         ROS_INFO("SetMethod request received. Method is: Euler");
     }
     if (config.integration_method == 1) {
-        integrator.setMethod(Integrator::Method::rk);
+        method = Method::rk;
         ROS_INFO("SetMethod request received. Method is: Runge_Kutta");
     }
 }
 
-/**
- * In main we create the node, the Integrator instance, launch the dynamic_reconfigure server, and spin.
- */
 int main(int argc, char **argv){
 
     ros::init(argc, argv, "integrator");
 
-    Integrator integrator;
-
-    dynamic_reconfigure::Server<scout_odometry::parametersConfig> server;
-    dynamic_reconfigure::Server<scout_odometry::parametersConfig>::CallbackType paramCallback;
-    paramCallback = boost::bind(&setMethod, boost::ref(integrator), _1, _2);
-    server.setCallback(paramCallback);
+    ROS_INFO("Starting integrator...");
+    IntegratorNode integrator;
     ROS_INFO("Integrator spinning...");
 
     ros::spin();
--- scout_odometry/src/synchronizer.cpp
+++ scout_odometry/src/synchronizer.cpp
@@ -1,102 +1,111 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "message_filters/subscriber.h"
-#include "message_filters/time_synchronizer.h"
-#include "geometry_msgs/TwistStamped.h"
-#include "robotics_hw1/MotorSpeed.h"
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <robotics_hw1/MotorSpeed.h>
 
-
-using namespace message_filters;
-
-
-
-class speed_node{
+/**
+ * This class represents the entire synchronizer node.
+ *
+ * It reads the motor speed from /motor_speed_XX,
+ * and when on 4 messages that satisfy the synchronization policy arrive it publishes on:
+ *  - scout_twist       geometry_msgs/TwistStamped message containing linear and angular speed of the robot
+ */
+class SynchronizerNode{
 
     public:
-        geometry_msgs::TwistStamped msg;
-        
+
+    SynchronizerNode();
 
     private:
 
-        double v_right, v_left; // speed of the right and left wheels in radiants/second
-        double v_x, w_z, v_y = 0; //velocity in y and in x
-        double y0 = 0.583; // half of the apparent base line
-        float gear_ratio =37.5;
-        
-        
-        ros::NodeHandle n; 
+    double v_right, v_left;     // average speed of the right and left wheels in radians/second
+    double v_x, w_z;            // linear and angular velocity
+    double y_0;                 // the apparent base line
+    float gear_ratio = 38.3;    // motor speed -> wheels gear ratio
 
-        //definition made to use the message filters
-        message_filters::Subscriber<robotics_hw1::MotorSpeed> front_l;
-        message_filters::Subscriber<robotics_hw1::MotorSpeed> front_r;
-        message_filters::Subscriber<robotics_hw1::MotorSpeed> rear_r;
-        message_filters::Subscriber<robotics_hw1::MotorSpeed> rear_l;
-        //definition of the filter policy
-        typedef message_filters::sync_policies::ExactTime<robotics_hw1::MotorSpeed,robotics_hw1::MotorSpeed,robotics_hw1::MotorSpeed,robotics_hw1::MotorSpeed> MySyncPolicy;
-        typedef Synchronizer<MySyncPolicy> Sync;
-        //definition in order to use a dynamic pointer
-        boost::shared_ptr<Sync> sync;
-        ros::Publisher pub_velocita;
-    
-    public:
-        //standard contructor
-        speed_node(){
-            front_l.subscribe(n, "/motor_speed_fl", 1);
-            front_r.subscribe(n, "/motor_speed_fr", 1);
-            rear_r.subscribe(n, "/motor_speed_rr", 1);
-            rear_l.subscribe(n, "/motor_speed_rl", 1);
-            pub_velocita = n.advertise<geometry_msgs::TwistStamped>("scout_twist", 1);
-            n.getParam("/apparent_baseline", y0);
+    ros::NodeHandle nodeHandle;
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> front_l;
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> front_r;
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> rear_l;
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> rear_r;
+    ros::Publisher twistPublisher;
 
-            sync.reset(new Sync(MySyncPolicy(10), front_l, front_r, rear_r, rear_l));
-            sync->registerCallback(boost::bind(&speed_node::speed_callback, this, _1,_2,_3,_4));
-    }
+    double rpmToAngular(double rpm);
+    double rpmToAvgSpeed(double v_f, double v_r);
+    void publishTwist(const robotics_hw1::MotorSpeed::ConstPtr& f_l, const robotics_hw1::MotorSpeed::ConstPtr& f_r,
+                      const robotics_hw1::MotorSpeed::ConstPtr& r_l, const robotics_hw1::MotorSpeed::ConstPtr& r_r);
 
-        //metods definitions
-
-        //subscriber callback
-        void speed_callback(const robotics_hw1::MotorSpeed::ConstPtr& f_l, const robotics_hw1::MotorSpeed::ConstPtr& f_r,
-                            const robotics_hw1::MotorSpeed::ConstPtr& r_r,const robotics_hw1::MotorSpeed::ConstPtr& r_l );
-        
-        private:
-        double calcolo_radiantis(double girim); // method that campute the speed in radiant/second
-        double calcolo_velocita_ruota(double v_f, double v_r); //method that compute the linear speed
+    typedef message_filters::sync_policies::ApproximateTime
+            <robotics_hw1::MotorSpeed,robotics_hw1::MotorSpeed,robotics_hw1::MotorSpeed,robotics_hw1::MotorSpeed>
+            MySyncPolicy;
+    message_filters::Synchronizer<MySyncPolicy> sync;
 };
 
-//method to tranform rmp in radiants/second
-double speed_node::calcolo_radiantis(double girim){
+/**
+ * The class constructor looks for the apparent baseline in the parameter server,
+ * sets up synchronization callback, subscribes to topics and advertises scout_twist.
+ */
+SynchronizerNode::SynchronizerNode()
+    : sync(MySyncPolicy(10), front_l, front_r, rear_l, rear_r)
+    , twistPublisher(nodeHandle.advertise<geometry_msgs::TwistStamped>("scout_twist", 1)){
 
-    return girim*2*3.141/60;  //rpm two times pi / 60
+    front_l.subscribe(nodeHandle, "/motor_speed_fl", 1);
+    front_r.subscribe(nodeHandle, "/motor_speed_fr", 1);
+    rear_l.subscribe(nodeHandle, "/motor_speed_rl", 1);
+    rear_r.subscribe(nodeHandle, "/motor_speed_rr", 1);
+    nodeHandle.getParam("apparent_baseline", y_0);
+    ROS_INFO("Read apparent baseline: %f", y_0);
+
+    sync.registerCallback(boost::bind(&SynchronizerNode::publishTwist, this, _1,_2,_3,_4));
 }
 
-//method that returns the speed of a single wheel
-double speed_node::calcolo_velocita_ruota(double v_f, double v_r){
+double SynchronizerNode::rpmToAngular(double rpm){
 
-    double  v_ruota;
-
-    v_ruota = (((calcolo_radiantis(v_f) + calcolo_radiantis(v_r))/2)/gear_ratio)*0.1575;// mean of front and rear velocity divided the gear ratio times the wheel's radius
-
-    return v_ruota;
+    return rpm * 2 * 3.141 / 60;
 }
 
-//subscriber's callback
-void speed_node::speed_callback(const robotics_hw1::MotorSpeed::ConstPtr& f_l, const robotics_hw1::MotorSpeed::ConstPtr& f_r,
-                            const robotics_hw1::MotorSpeed::ConstPtr& r_r,const robotics_hw1::MotorSpeed::ConstPtr& r_l ){
-    v_left = speed_node::calcolo_velocita_ruota(f_l->rpm,  r_l->rpm); //left and right speed
-    v_right = speed_node::calcolo_velocita_ruota(f_r->rpm,  r_r->rpm);
+/**
+ * Takes the rpm of two AgileX Scout 2.0 motors and returns the average linear speed of the two wheels.
+ * It takes into account the wheel radius and the gear ratio.
+ *
+ * @param rpm1      First motor rpm
+ * @param rpm2      Second motor rpm
+ * @return          Average speed of the two wheels
+ */
+double SynchronizerNode::rpmToAvgSpeed(double rpm1, double rpm2){
 
-    v_left = -v_left;
+    return (rpmToAngular(rpm1) + rpmToAngular(rpm2)) / 2 / gear_ratio * 0.1575;
+}
 
-    v_x = (v_left + v_right)/2;       //linear velocity in x
-    w_z = (-v_left+v_right)/(2*y0);   //angular velocity
+/**
+ * Callback method for synchronized motor speed messages.
+ * Publishes the linear and angular twist of the robot on topic scout_twist.
+ *
+ * @param f_l       Front left motor speed message
+ * @param f_r       Front right motor speed message
+ * @param r_l       Rear left motor speed message
+ * @param r_r       Rear right motor speed message
+ */
+void SynchronizerNode::publishTwist(const robotics_hw1::MotorSpeed::ConstPtr& f_l, const robotics_hw1::MotorSpeed::ConstPtr& f_r,
+                                       const robotics_hw1::MotorSpeed::ConstPtr& r_l, const robotics_hw1::MotorSpeed::ConstPtr& r_r ){
 
+    v_left = rpmToAvgSpeed(f_l->rpm, r_l->rpm);
+    v_right = rpmToAvgSpeed(f_r->rpm, r_r->rpm);
+
+    v_left = - v_left;                  // takes into account the fact that in the provided bags
+                                        // the left motor speeds are negative when moving forward
+
+    v_x = (v_left + v_right) / 2;       // linear velocity in x
+    w_z = (-v_left + v_right) / y_0;    // angular velocity
+
+    geometry_msgs::TwistStamped msg;
     msg.header.stamp = ros::Time::now();
-    msg.twist.linear.x = v_x;         //velocity message
+    msg.twist.linear.x = v_x;
     msg.twist.angular.z = w_z;
-    msg.twist.linear.y = v_y;
-       
-    pub_velocita.publish(msg);        //publishing
-
+    twistPublisher.publish(msg);
 }
 
 
@@ -104,11 +113,11 @@ int main(int argc, char **argv){
 
     ros::init(argc, argv, "synchronizer");
 
-    speed_node sub_pub;     //object creation 
-
+    ROS_INFO("Starting synchronizer...");
+    SynchronizerNode synchronizer;
     ROS_INFO("Synchronizer spinning...");
+
     ros::spin();
 
     return 0;
-
 }
